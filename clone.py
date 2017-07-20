e4cb15f4f38d532275510c2b203e0e8fed58ffaf
Vivek Chand <vivekchand19@gmail.com>
2017-07-21 02:00:08 +0530

    try Nvidia Network architecture

--- clone.py
+++ clone.py
@@ -1,4 +1,5 @@
 import csv
+
 import cv2
 import numpy as np
 
@@ -36,21 +37,22 @@ y_train = np.array(augmented_measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, nb_epoch=2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, nb_epoch=3, shuffle=True)
 
 model.save('model.h5')
