293be7698d03d991fd7efe6cc3118fce66762cee
Vivek Chand <vivekchand19@gmail.com>
2017-07-24 08:41:41 +0530

    decrease samples per epoch

--- model.py
+++ model.py
@@ -53,7 +53,6 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
@@ -69,6 +68,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples) / 3, validation_data=validation_generator,
                     nb_val_samples=len(validation_samples), nb_epoch=3)
 model.save('model.h5')
