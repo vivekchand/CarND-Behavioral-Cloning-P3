1f6b195e56a1939dca044bc42ce1e022d67decc1
Vivek Chand <vivekchand19@gmail.com>
2017-07-24 09:50:40 +0530

    fix sample epoch

--- model.py
+++ model.py
@@ -67,6 +67,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples) / 3, validation_data=validation_generator,
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
                     nb_val_samples=len(validation_samples), nb_epoch=3)
 model.save('model.h5')
